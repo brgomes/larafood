e35c3bfdd4bce1b4cc5d7db811adf8c89173c55d
Bruno Roberto Gomes <bruno@brgomes.com>
2022-05-17 21:44:34 -0300

    Módulos 1 e 2 ok

---
+++ resources/views/admin/pages/plans/edit.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', "Editar Plano {$plan->name}")
+
+@section('content_header')
+    <h1>Editar Plano <b>{{ $plan->name }}</b></h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <form action="{{ route('plans.update', $plan->url) }}" method="POST" class="form">
+                @csrf
+                @method('PUT')
+
+                @include('admin.pages.plans.partials.form')
+            </form>
+        </div>
+    </div>
+@stop
